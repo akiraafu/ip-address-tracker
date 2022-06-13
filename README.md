167434c241fb78cfa78b456f784598afdac1c302
Junrong <87742302+akiraafu@users.noreply.github.com>
2022-06-13 13:24:20 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
-# ip-address-tracker
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Screenshot](#screenshot)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+    -   [What I learned](#what-i-learned)
+    -   [Continued development](#continued-development)
+    -   [Useful resources](#useful-resources)
+-   [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+-   View the optimal layout for each page depending on their device's screen size
+-   See hover states for all interactive elements on the page
+-   See their own IP address on the map on the initial page load
+-   Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./1.png)
+![](./2.png)
+
+### Links
+
+-   Solution URL: [https://github.com/akiraafu/ip-address-tracker]
+-   Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   CSS custom properties
+-   Flexbox
+-   JavaScript
+-   Fetch
+-   async await
+
+### What I learned
+
+```css
+.proud-of-this-css {
+    .header {
+        min-height: 34vh;
+    }
+    @media (max-width: 768px) {
+        .header .outputs {
+            height: 100%;
+            min-height: 15rem;
+            flex-direction: column;
+            top: 10rem;
+            overflow: visible;
+        }
+    }
+}
+```
+
+```js
+const proudOfThisFunc = () => {
+    const getGeoIp = fetch(API_URL)
+        .then((response) => response.json())
+        .then((ip) => {
+            return [ip.location, ip.isp, ip.ip];
+        });
+
+    const printAddress = async () => {
+        const a = await getGeoIp;
+        // console.log(a);
+
+        let location = a[0];
+        let getRegion = location["region"];
+        let getCity = location["city"];
+        let timezone = location["timezone"];
+        .
+        .
+        .
+    };
+};
+```
+
+### Continued development
+
+none
+
+### Useful resources
+
+-   [MDN](https://developer.mozilla.org/en-US/)
+-   [Stackoverflow](https://stackoverflow.com/)
+-   [IP Geolocation API Docs](https://geo.ipify.org/docs)
+-   [leafletjs Docs](https://leafletjs.com/reference.html)
+
+## Author
+
+-   Website - [akiraafu](https://github.com/akiraafu)
+-   Frontend Mentor - [@akiraafu](https://www.frontendmentor.io/profile/akiraafu)
--- app.js
+++ app.js
@@ -6,51 +6,104 @@ let locationOutput = document.querySelector(".location");
 let timezoneOutput = document.querySelector(".timezone");
 let ispOutput = document.querySelector(".isp");
 
+// See their own IP Address on the map on the initial page load
+const initialPage = () => {
+    let API_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=at_Fjg3Ql55BRq7aCffMc1fEcYzgxkjd&ipAddress=`;
+    const getGeoIp = fetch(API_URL)
+        .then((response) => response.json())
+        .then((ip) => {
+            return [ip.location, ip.isp, ip.ip];
+        });
+
+    const printAddress = async () => {
+        const a = await getGeoIp;
+        // console.log(a);
+
+        let location = a[0];
+        let getRegion = location["region"];
+        let getCity = location["city"];
+        let timezone = location["timezone"];
+        timezoneOutput.innerHTML = `UTC ${timezone}`;
+
+        locationOutput.innerHTML = `${getCity}, ${getRegion} `;
+
+        let isp = a[1];
+        ispOutput.innerHTML = `${isp}`;
+
+        let ip = a[2];
+        ipAddress.innerHTML = `${ip}`;
+
+        let lat = location["lat"];
+        let lng = location["lng"];
+
+        // map
+        let map = L.map("map").setView([lat, lng], 13);
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: "© OpenStreetMap",
+        }).addTo(map);
+        let icon = L.icon({
+            iconUrl: "./images/icon-location.svg",
+            iconSize: [36, 46], // size of the icon
+        });
+        let marker = L.marker([lat, lng], { icon: icon }).addTo(map);
+    };
+
+    printAddress();
+};
+
+initialPage();
+
+// use userInput
 button.addEventListener("click", (e) => {
     e.preventDefault();
     const inputValue = input.value;
-
     ipAddress.innerHTML = inputValue;
+
+    let API_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=at_Fjg3Ql55BRq7aCffMc1fEcYzgxkjd&ipAddress=${inputValue}`;
+
+    const getGeoIp = fetch(API_URL)
+        .then((response) => response.json())
+        .then((ip) => {
+            return [ip.location, ip.isp];
+        });
+
+    const printAddress = async () => {
+        const a = await getGeoIp;
+        // console.log(a);
+
+        let location = a[0];
+        let getRegion = location["region"];
+        let getCity = location["city"];
+        let timezone = location["timezone"];
+        timezoneOutput.innerHTML = `UTC ${timezone}`;
+
+        locationOutput.innerHTML = `${getCity}, ${getRegion} `;
+
+        let isp = a[1];
+        ispOutput.innerHTML = `${isp}`;
+
+        let lat = location["lat"];
+        let lng = location["lng"];
+
+        // map
+        if (L.DomUtil.get("map") !== undefined) {
+            L.DomUtil.get("map")._leaflet_id = null;
+        }
+
+        let map = L.map("map").setView([lat, lng], 13);
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: "© OpenStreetMap",
+        }).addTo(map);
+        let icon = L.icon({
+            iconUrl: "./images/icon-location.svg",
+            iconSize: [36, 46], // size of the icon
+        });
+        let marker = L.marker([lat, lng], { icon: icon }).addTo(map);
+    };
+
     printAddress();
 });
-
-const inputValue = input.value;
-let API_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=at_Fjg3Ql55BRq7aCffMc1fEcYzgxkjd&ipAddress=${inputValue}`;
-
-const getGeoIp = fetch(API_URL)
-    .then((response) => response.json())
-    .then((ip) => {
-        return [ip.location, ip.isp];
-    })
-    .then(function (result) {
-        return result;
-    });
-
-const printAddress = async () => {
-    const a = await getGeoIp;
-    // console.log(a);
-
-    let location = a[0];
-    // console.log(location);
-    let getRegion = location["region"];
-    let getCity = location["city"];
-    let timezone = location["timezone"];
-    timezoneOutput.innerHTML = `UTC${timezone}`;
-
-    locationOutput.innerHTML = `${getCity}, ${getRegion} `;
-
-    let isp = a[1];
-    ispOutput.innerHTML = `${isp}`;
-
-    let lat = location["lat"];
-    let lng = location["lng"];
-    // map
-    let map = L.map("map").setView([lat, lng], 13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution: "© OpenStreetMap",
-    }).addTo(map);
-
-    let marker = L.marker([lat, lng]).addTo(map);
-};
